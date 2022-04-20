e320e7497d3e6acfee261e8b16a647dc95733a15
khesya Amelia Putri <khesyaap0209@gmail.com>
2022-04-20 13:28:55 +0700

    changed to stateless

--- lib/screen/favorite_screen.dart
+++ lib/screen/favorite_screen.dart
@@ -10,7 +10,7 @@ class FavoriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if(favoriteMeals.isEmpty){
-      return Center(
+      return const Center(
         child: Text('Anda tidak memiliki data favorite'),
       );
     }else {
